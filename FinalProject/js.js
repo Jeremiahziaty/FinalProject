2c36e0e3cc986a5da00c8fcd3d659497b19ed950
suppastar\jeremiah <ziatj01@students.ipfw.edu>
2016-10-30 22:40:53 -0600

    i added a play button in my rules page that is a link. the button sends the player to the input page where he is ask for firstname lastname and to choose if they want the ace to be counted as one or eleven

---
+++ FinalProject/js.js
@@ -0,0 +1,8 @@
+/**
+ * Created by jeremiah on 10/30/2016.
+ */
+function MyFunction() {
+    var first, second;
+    first = document.getElementById("firstname").value;
+    second = document.getElementById("lastname").value;
+}
